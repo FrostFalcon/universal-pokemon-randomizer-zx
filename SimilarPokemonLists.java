5163dfa09f0ce7620204e09d27bc4c6bf9b5e40f
FrostFalcon <81112174+FrostFalcon@users.noreply.github.com>
2021-03-21 15:39:07 -0400

    Add files via upload

---
+++ SimilarPokemonLists.java
@@ -0,0 +1,13 @@
+package com.dabomstew.pkrandom.constants;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class SimilarPokemonLists {
+    
+    public static final List<String> grassStarters = Collections.unmodifiableList(Arrays.asList("Bulbasaur", "Chikorita", "Treecko", "Turtwig", "Snivy"));
+    public static final List<String> fireStarters = Collections.unmodifiableList(Arrays.asList("Charmander", "Cyndaquil", "Torchic", "Chimchar", "Tepig"));
+    public static final List<String> waterStarters = Collections.unmodifiableList(Arrays.asList("Squirtle", "Totodile", "Mudkip", "Piplup", "Oshawott"));
+
+}
